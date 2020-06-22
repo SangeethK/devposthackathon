d258c8f4994eb407457129e648f67ea632b3c29b
SangeethK <sangeet.konumuri@gmail.com>
2020-06-22 09:01:50 +0530

    more enhancements

--- scripts/script.js
+++ scripts/script.js
@@ -14,11 +14,52 @@
 //==============================================================================
 
 // How to load in modules
+import * as lib from './utils'
 const Scene = require('Scene');
 const FaceTracking = require('FaceTracking');
 const Animation = require('Animation');
 const TouchGestures = require('TouchGestures')
 const NativeUI = require('NativeUI')
+const Textures = require('Textures');
+const Patches = require('Patches');
+const Diagnostics = require('Diagnostics');
+
+Promise.all([
+    Textures.findFirst('icon_1'),
+    Textures.findFirst('icon_2'),
+    Textures.findFirst('icon_3'),
+]).then(onReady);
+
+// Log a string message  
+//Diagnostics.log('Loading');
+
+function onReady(assets) {    
+    const texture0 = assets[0];
+    const texture1 = assets[1];
+    const texture2 = assets[2];
+    const picker = NativeUI.picker;
+
+    const index = 0;
+    const selection = 0;
+
+    const configuration = {
+
+      selectedIndex: index,
+
+      items: [
+        {image_texture: texture0},
+        {image_texture: texture1},
+        {image_texture: texture2}
+      ]
+    };
+
+    picker.configure(configuration);
+    picker.visible = true;
+
+    picker.selectedIndex.monitor().subscribe(function(index) {
+      Patches.inputs.setScalar('selection', index.newValue);
+    });
+}
 
 Scene.root.findFirst('Heart', {recursive: true}).then(function(base){
     const baseDriverParameters = {
@@ -41,10 +82,24 @@ Scene.root.findFirst('Heart', {recursive: true}).then(function(base){
     baseTransform.scaleZ = baseAnimation;
 })
 
-Scene.root.findFirst('2dText0', {recursive: true}).then(function(base){
-    TouchGestures.onTap(base).subscribe(function() {
-        NativeUI.enterTextEditMode(base);
-    });      
-})
+Scene.root.findFirst('emitter0', {recursive: true}).then(function(base){
+    const baseDriverParameters = {
+        durationMilliseconds: 400, 
+        loopCount: Infinity, 
+        mirror: true
+    }
+    
+    const baseDriver = Animation.timeDriver(baseDriverParameters);
+    baseDriver.start();
 
+    const baseSampler = Animation.samplers.easeInQuint(0.9, 1)
+
+    const baseAnimation = Animation.animate(baseDriver, baseSampler)
+
+    const baseTransform = base.transform;
+
+    baseTransform.scaleX = baseAnimation; 
+    baseTransform.scaleY = baseAnimation;
+    baseTransform.scaleZ = baseAnimation;
+})
 
---
+++ scripts/utils.js
@@ -0,0 +1,11 @@
+
+var color = 10
+
+// To make symbols available to other files or scripting console
+
+// Use export keyword
+// export function functionName() {}
+// export class ClassName {}
+export default function randomGenerator() {
+    return Math.random;
+}
